9d8feb2410204db1fa8b339fe1bcaa3412e74317
humbertovacu <humbertovacu@gmail.com>
2022-11-14 16:55:58 +0100

    iteration 3 completed

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,4 +1,18 @@
+const Celebrity = require('../models/Celebrity.model');
+
 const router = require('express').Router();
 
+
+router.get('/create',(req, res)=>
+    res.render('celebrities/new-celebrity.hbs')
+)
+
+router.post('/create', (req, res)=> {
+ const { name, occupation, catchPhrase } = req.body;
+ Celebrity.create({name, occupation, catchPhrase})
+ .then (createdCeleb => res.redirect('/celebrities'))
+ .catch (err => res.render('celebrities/new-celebrity.hbs'));
+}
+)
 
 module.exports = router;
