f2d8a3e2470cd42f37d8463c53c5aa3e2f2e73fd
humbertovacu <humbertovacu@gmail.com>
2022-11-14 18:20:38 +0100

    iteration 6 completed

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 const Movie = require('../models/Movies.model');
+const Celebrity = require('../models/Celebrity.model');
 
+router.get('/create',(req,res)=>{
+    Celebrity.find()
+    .then(allCelebrities => res.render('movies/new-movie.hbs', {celebrities: allCelebrities}))
+})
+
+router.post('/create',(req,res)=>{
+    const { title, genre, plot, cast } = req.body;
+    Movie.create({title, genre, plot, cast})
+     .then (() => res.redirect('/movies'))
+     .catch(err => console.log(err))
+})
+
+router.get('/', (req,res)=>{
+    res.render('movies/movies.hbs')
+})
 
 module.exports = router;
